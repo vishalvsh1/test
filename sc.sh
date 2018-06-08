33562ac897f221b60473512350cb98604e86081b
Vishal <vishal_vsh1@yahoo.com>
2018-06-08 19:14:12 +0530

    Update sc.sh

--- sc.sh
+++ sc.sh
@@ -1,45 +1,39 @@
-#!/bin/bash -x
-
-sudo add-apt-repository ppa:webupd8team/java
-sudo apt-get update
-
-sudo apt-get install oracle-java8-installer
-sudo apt-get install oracle-java8-set-default
-
-cd /opt
-sudo wget "http://mirror.cc.columbia.edu/pub/software/apache/kafka/0.11.0.2/kafka_2.11-0.11.0.2.tgz" -O kafka.tgz
-sudo tar -xvzf kafka.tgz
-sudo rm kafka.tgz
-# symbolic link created so that the folder name is memorable
-sudo ln -s kafka_2.11-0.11.0.2 kafka
-
-
-
-max.request.size=104857600
-message.max.size=104857600
-fetch.message.max.bytes=104857600
-replica.fetch.max.bytes=104857600
-log.dirs = /data/kafka/logs
-
-
-autopurge.purgeInterval=24
-autopurge.snapRetainCount=5
-
-
-cd /opt/kafka
+#!/bin/bash
+### BEGIN INIT INFO
+# Provides:          disable-transparent-hugepages
+# Required-Start:    $local_fs
+# Required-Stop:
+# X-Start-Before:    mongod mongodb-mms-automation-agent
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Disable Linux transparent huge pages
+# Description:       Disable Linux transparent huge pages, to improve
+#                    database performance.
+### END INIT INFO
+case $1 in
+  start)
+    if [ -d /sys/kernel/mm/transparent_hugepage ]; then
+      thp_path=/sys/kernel/mm/transparent_hugepage
+    elif [ -d /sys/kernel/mm/redhat_transparent_hugepage ]; then
+      thp_path=/sys/kernel/mm/redhat_transparent_hugepage
+    else
+      return 0
+    fi
  
-nohup bin/zookeeper-server-start.sh config/zookeeper.properties > zookeeper.log 2>&1 &
-nohup bin/kafka-server-start.sh config/server.properties > kafka.log 2>&1 &
-
-#mongo
-
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
-sudo apt-get update
-
-
-sudo apt-get install -y mongodb-org
-
-
-sudo chown -R mongodb:mongodb /data/mongo
-
+    echo 'never' > ${thp_path}/enabled
+    echo 'never' > ${thp_path}/defrag
+ 
+    re='^[0-1]+$'
+    if [[ $(cat ${thp_path}/khugepaged/defrag) =~ $re ]]
+    then
+      # RHEL 7
+      echo 0  > ${thp_path}/khugepaged/defrag
+    else
+      # RHEL 6
+      echo 'no' > ${thp_path}/khugepaged/defrag
+    fi
+ 
+    unset re
+    unset thp_path
+    ;;
+esac
