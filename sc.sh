9759cde76e0c503dcfbf285a34d933cbdaf1aa8b
Vishal <vishal_vsh1@yahoo.com>
2018-06-08 18:48:33 +0530

    Update sc.sh

--- sc.sh
+++ sc.sh
@@ -1,20 +1,45 @@
 #!/bin/bash -x
 
-# this deb file will allow us to add the necessary erlang and rabbitmq repositories entries to the 'apt' list
-echo "deb https://packages.erlang-solutions.com/ubuntu xenial contrib" | sudo tee /etc/apt/sources.list.d/erlang-solutions.list
-wget --quiet -O - https://packages.erlang-solutions.com/ubuntu/erlang_solutions.asc | sudo apt-key add -
-echo "deb https://dl.bintray.com/rabbitmq/debian xenial main" | sudo tee /etc/apt/sources.list.d/bintray.rabbitmq.list
-wget -O- https://dl.bintray.com/rabbitmq/Keys/rabbitmq-release-signing-key.asc | sudo apt-key add -
-
-
-
-
+sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
-sudo apt-get install -y esl-erlang
-sudo apt-get install -y rabbitmq-server
+
+sudo apt-get install oracle-java8-installer
+sudo apt-get install oracle-java8-set-default
+
+cd /opt
+sudo wget "http://mirror.cc.columbia.edu/pub/software/apache/kafka/0.11.0.2/kafka_2.11-0.11.0.2.tgz" -O kafka.tgz
+sudo tar -xvzf kafka.tgz
+sudo rm kafka.tgz
+# symbolic link created so that the folder name is memorable
+sudo ln -s kafka_2.11-0.11.0.2 kafka
 
 
-sudo rabbitmqctl add_user zimperium <my_super_secret_password>
-sudo rabbitmqctl set_permissions -p / zimperium '.*' '.*' '.*'
-sudo rabbitmqctl set_user_tags zimperium administrator
-sudo rabbitmq-plugins enable rabbitmq_management
+
+max.request.size=104857600
+message.max.size=104857600
+fetch.message.max.bytes=104857600
+replica.fetch.max.bytes=104857600
+log.dirs = /data/kafka/logs
+
+
+autopurge.purgeInterval=24
+autopurge.snapRetainCount=5
+
+
+cd /opt/kafka
+ 
+nohup bin/zookeeper-server-start.sh config/zookeeper.properties > zookeeper.log 2>&1 &
+nohup bin/kafka-server-start.sh config/server.properties > kafka.log 2>&1 &
+
+#mongo
+
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
+sudo apt-get update
+
+
+sudo apt-get install -y mongodb-org
+
+
+sudo chown -R mongodb:mongodb /data/mongo
+
